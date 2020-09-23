1d251cae678de17a537230c8db054ee2321ce08b
ocozalp <ozalp.orhan@gmail.com>
2020-09-23 00:46:59 -0400

    docker-compose with network

--- message-producer/main.go
+++ message-producer/main.go
@@ -3,24 +3,26 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/go-redis/redis/v8"
 	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
 )
-import "github.com/google/uuid"
 
 func main() {
 	fmt.Println("Creating client")
-	client := redis.NewClient(&redis.Options{Addr: ":12345"})
+	client := redis.NewClient(&redis.Options{Addr: "redis:6379"})
 	ctx := context.Background()
 	for {
 		uuid := uuid.New().String()
-		err := client.Set(ctx, "test_uuid", uuid, time.Hour)
+		result := client.Set(ctx, "test_uuid", uuid, time.Hour)
 
-		if err != nil {
-			fmt.Printf("Error: %v\n", err)
-			break
+		if result.Err() != nil {
+			fmt.Printf("Error: %v\n", result.Err())
+		} else {
+			fmt.Println("Key successfully updated...")
 		}
 
-		time.Sleep(time.Second*5)
+		time.Sleep(time.Second * 5)
 	}
 }
